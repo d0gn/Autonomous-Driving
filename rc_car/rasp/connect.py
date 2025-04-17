200317afc110834f267da73e7da68f7b157cb268
sick2024471 <126737248+sick2024471@users.noreply.github.com>
2025-04-17 17:46:34 +0900

    Update connect.py

--- rc_car/rasp/connect.py
+++ rc_car/rasp/connect.py
@@ -1,25 +1,28 @@
-import cv2
-import requests
+import cv2, base64
+import socketio
 import time
 
-SERVER_URL = 'http://<서버 IP>:5000/upload_frame1'
+sio = socketio.Client()
+@sio.event
+def connect():
+    sio.emit('start_stream')
 
-cap = cv2.VideoCapture('sample.mp4')
+def stream_loop():
+    cap = cv2.VideoCapture('sample.mp4')
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        b64 = base64.b64encode(buffer).decode('utf-8')
+        sio.emit('video_frame', b64)
 
-    _, buffer = cv2.imencode('.jpg', frame)
-    files = {'frame': ('frame.jpg', buffer.tobytes(), 'image/jpeg')}
+        sio.sleep(0.03)
 
-    try:
-        response = requests.post(SERVER_URL, files=files, timeout=1)
-        print('Sent frame. Server responded:', response.text)
-    except requests.exceptions.RequestException as e:
-        print('Error sending frame:', e)
+    cap.release()
 
-    time.sleep(0.1)
-
-cap.release()
+# 메인 실행
+if __name__ == '__main__':
+    sio.connect('http://127.0.0.1:5000')  # 또는 main_server의 IP
+    stream_loop()
